96f24fb6be59f5683c5123e0aa62fc4a990ace92
Shelby Salas <138728037+s-salas@users.noreply.github.com>
2024-04-22 16:35:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,458 @@
+const categories = ["Category 1", "Category 2", "Category 3", "Category 4", "Category 5", "Category 6"];
+const questions = [
+  [
+    // category 1
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+  ],
+  [
+    // category 2
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+  ],
+  [
+    // category 3
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+  ],
+  [
+    // category 4
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+  ],
+  [
+    // category 5
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+  ],
+  [
+    // category 6
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+    "The question will be read and you'll have 20 seconds to answer.", // answer
+  ],
+];
+
+const answers = [
+  [
+    // category 1
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+  ],
+  [
+    // category 2
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+  ],
+  [
+    // category 3
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+  ],
+  [
+    // category 4
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+    "The answer will be shown here.",
+  ],
+ [
+  // category 5
+  "The answer will be shown here.",
+  "The answer will be shown here.",
+  "The answer will be shown here.",
+  "The answer will be shown here.",
+  "The answer will be shown here.",
+],
+ [
+  // category 6
+  "The answer will be shown here.",
+  "The answer will be shown here.",
+  "The answer will be shown here.",
+  "The answer will be shown here.",
+  "The answer will be shown here.",
+],
+]
+
+const hints = [
+  [
+    // category 1
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+  ],
+  [
+    // category 2
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+  ],
+  [
+    // category 3
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+  ],
+  [
+    // category 4
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+  ],
+  [
+    // category 5
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+  ],
+  [
+    // category 6
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+    "A hint will be given, but you'll only get half the points :(",
+  ]
+]
+
+const multipleChoice = [
+  [
+    // category 1
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+  ],
+  [
+    // category 2
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+  ],
+  [
+    // category 3
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+  ],
+  [
+    // category 4
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+  ],
+  [
+    // category 5
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+  ], 
+  [
+    // category 6
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+    "You can get multiple choice answers... but you'll only get a quarter of the points :(  :(",
+  ],
+]
+
+const players = [
+  ["Eugene", 0],
+  ["Evonn", 0],
+  ["Kayla", 0],
+  ["Lily", 0],
+  ["Luis", 0],
+  ["Mary", 0],
+  ["Michael", 0],
+  ["Patty", 0]
+];
+
+$(document).ready(function () {
+  const startBtn = document.getElementById('start-btn');
+  const stopBtn = document.getElementById('stop-btn');
+  const resetBtn = document.getElementById('reset-btn');
+
+let timer;
+let milliseconds = 0;
+let seconds = 20;
+
+// set up timer
+
+startBtn.addEventListener('click', function () { 
+  // Start the stopwatch only if it's not already running
+  if (!timer) {
+    timer = setInterval(updateStopwatch, 10); // Update the stopwatch every 10 milliseconds
+  }
+});
+
+stopBtn.addEventListener('click', function () { 
+  // Stop the stopwatch
+  clearInterval(timer);
+  timer = null; // Reset the timer variable
+});
+
+resetBtn.addEventListener('click', function () { 
+  // Reset the stopwatch
+  clearInterval(timer);
+  timer = null;
+  milliseconds = 0;
+  seconds = 20;
+  updateDisplay();
+});
+
+function updateStopwatch() { 
+  // decrement milliseconds
+  milliseconds -= 10;
+
+  // Check if milliseconds reach zero
+  if (milliseconds < 0) {
+    milliseconds = 990;
+    seconds--;
+  }
+
+  // Check if the timer reaches zero
+  if (seconds === 0 && milliseconds === 0) {
+    clearInterval(timer);
+    timer = null;
+  }
+
+  // Update the display
+  updateDisplay();
+}
+
+function updateDisplay() {
+  // Calculate total milliseconds
+  let totalMilliseconds = (seconds * 1000) + milliseconds;
+
+  // Calculate remaining seconds and milliseconds
+  let remainingSeconds = Math.floor(totalMilliseconds / 1000);
+  let remainingMilliseconds = totalMilliseconds % 1000;
+
+  // Display remaining seconds and milliseconds with leading zeros
+  const secString = remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds;
+  const msString = remainingMilliseconds < 100 ? '0' + remainingMilliseconds : remainingMilliseconds.toString().padStart(3, '0');
+
+  // Update the HTML element with id 'stopwatch' with the formatted time
+  document.getElementById('stopwatch').textContent = secString + '.' + msString;
+}
+
+});
+
+function setupBoard() {
+  for (let i = 0; i < categories.length; i++) {
+    let col = document.createElement("div");
+    col.className = "col-sm text-center";
+    let card = document.createElement("div");
+    card.className = "card";
+
+    const cardHeader = document.createElement("div");
+    cardHeader.className = "card-header";
+    const header = document.createElement("h2");
+    const headerText = document.createTextNode(categories[i]);
+    header.appendChild(headerText);
+
+    const list = document.createElement("ul");
+    list.className = "list-group list-group-flush";
+    for (let j = 0; j < questions[i].length; j++) {
+      const link = document.createElement("a");
+      link.setAttribute("href", "");
+      link.setAttribute("data-toggle", "modal");
+      link.setAttribute("data-target", "#questionModal");
+      link.setAttribute("data-category", i.toString());
+      link.setAttribute("data-points", ((j + 1) * 100).toString());
+      link.setAttribute("data-questionid", j.toString());
+      const listItem = document.createElement("li");
+      listItem.className = "list-group-item";
+      const amount = document.createTextNode((j + 1) * 100);
+      listItem.appendChild(amount);
+
+      list.append(link);
+      link.append(listItem);
+    }
+
+    $("#gameBoard").append(col);
+    col.append(card);
+    card.append(cardHeader);
+    cardHeader.append(header);
+    card.append(list);
+  }
+}
+
+function setScoreboard() {
+  $("#scoreboard").empty();
+  players.forEach(function (player) {
+    let s = "<p class='player-text'>" + player[0] + ": " + player[1] + "</p>";
+    $("#scoreboard").append(s);
+  });
+}
+
+function nextQuestion() {
+  $("#questionModal").modal("hide");
+  setScoreboard();
+}
+
+$(document).ready(function () {
+  let category = "";
+  let points = "";
+  let questionid = "";
+
+  setupBoard();
+  setScoreboard();
+
+  $("a").click(function () {
+    $(this).addClass("opacity");
+    $(this).children().addClass("opacity");
+  });
+
+  $("#questionModal").on("shown.bs.modal", function (event) {
+    let link = $(event.relatedTarget);
+    category = link.data("category");
+    points = link.data("points");
+    questionid = link.data("questionid");
+
+    let modal = $(this);
+    modal.find(".modal-title").text(categories[category] + " for " + points + " points");
+    modal.find(".modal-body p").text(questions[category][questionid]);
+    $("#answer-btn").show(); // show the answer button
+    $("#hint-btn").show(); // show the hint button
+    $("#stopwatch-btns").show(); // show the stopwatch
+    $("#player-btns").hide(); // hide the player buttons initially
+    $("#multiple-btn").hide(); // hide the multiple choice button initially
+    $("#incorrect").hide(); // hide incorrect button initially
+  });
+
+  $("#answer-btn").click(function () {
+    let modal = $("#questionModal");
+    modal.find(".modal-body p").text(answers[category][questionid]); // show the answer
+    $("#answer-btn").hide(); // hide the answer button
+    $("#hint-btn").hide(); // hide the hint button
+    $("#multiple-btn").hide(); // hide the multiple choice button
+    $("#stopwatch-btns").hide(); // hide the stopwatch
+    $("#player-btns").show(); // show the player buttons
+    $("#incorrect").show(); // show the incorrect button
+  });
+
+  $("#hint-btn").click(function () {
+    let modal = $("#questionModal");
+    let halfPoints = Math.floor(parseInt(points) / 2); // calculate half the points
+    let questionText = (questions[category][questionid]); // get the question text
+    let hintText = hints[category][questionid]; // get hint text
+    let questionPara = $("<p>").text(questionText);
+    let hintPara = $("<p>").text("Hint: " + hintText);
+    modal.find(".modal-body p").empty().append(questionPara, hintPara); // show question and hint
+    $("#hint-btn").hide(); // hide the hint button
+    $("#answer-btn").show(); // show the answer button
+    $("#multiple-btn").show(); // show the multiple choice button
+    points = halfPoints; // update points awarded to half
+  });
+
+  $("#multiple-btn").click(function () {
+    let modal = $("#questionModal");
+    let quarterPoints = Math.floor(parseInt(points) / 2); // calculate quarter of the points
+    let questionText = (questions[category][questionid]); // get the question text
+    let hintText = hints[category][questionid]; // get hint text
+    let questionPara = $("<p>").text(questionText);
+    let hintPara = $("<p>").text("Hint: " + hintText);
+    let choices = multipleChoice[category][questionid].split(', '); // split the multiple choices by comma and space
+    let choicesHtml = ""; // initialize an empty string to store HTML for choices
+    choices.forEach(choice => {
+      choicesHtml += "<p>" + choice + "</p>"; // create paragraph element for each choice
+    });
+    modal.find(".modal-body p").empty().append(questionPara, hintPara, choicesHtml); // show multiple choices
+    $("#multiple-btn").hide(); // hide the multiple choice button
+    $("#answer-btn").show(); // show the answer button
+    points = quarterPoints; // update points awarded to a quarter
+  });
+
+  // modify player button click handlers to update score after answer is revealed
+  $("#player-1").click(function () {
+    players[0][1] += parseInt(points);
+    nextQuestion();
+  });
+
+  $("#player-2").click(function () {
+    players[1][1] += parseInt(points);
+    nextQuestion();
+  });
+
+  $("#player-3").click(function () {
+    players[2][1] += parseInt(points);
+    nextQuestion();
+  });
+
+  $("#player-4").click(function () {
+    players[3][1] += parseInt(points);
+    nextQuestion();
+  });
+
+  $("#player-5").click(function () {
+    players[4][1] += parseInt(points);
+    nextQuestion();
+  });
+
+  $("#player-6").click(function () {
+    players[5][1] += parseInt(points);
+    nextQuestion();
+  });
+
+  $("#player-7").click(function () {
+    players[6][1] += parseInt(points);
+    nextQuestion();
+  });
+
+  $("#player-8").click(function () {
+    players[7][1] += parseInt(points);
+    nextQuestion();
+  });
+
+  $("#incorrect").click(function () {
+    nextQuestion();
+  });
+});
